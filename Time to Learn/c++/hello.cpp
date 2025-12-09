249bb3cd819e20b3ab0dce11a0991b976e20d5e5
Abdullah Al Mamun <252-15-949@diu.edu.bd>
2025-12-09 11:39:34 +0600

    Create Time to Learn file

---
+++ Time to Learn/c++/hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDG
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int a,b;
+    cin>>a>>b;
+    cout<<"heloo "<<a+b<<"\n";
+    return 0;
+}
